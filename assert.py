5290b090ff9a409fadc973fca2eb86aa0829cb57
prajun v <prajunvelandi@gmail.com>
2024-05-05 19:44:29 +0530

    added assert file

---
+++ assert.py
@@ -0,0 +1,6 @@
+a = 2
+b = 3
+assert a == b, 'a is not equal to b'
+
+# assert condition, 'message'
+# If the condition is satisfied it will raise an assertion error
--- main.py
+++
@@ -1,2 +0,0 @@
-s = 'hello world    fhh    okfjf'
-print(s.split(' '))
