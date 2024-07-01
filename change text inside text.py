70363cb63c98c248eb4b0f5636311aeb1cb62017
prajun v <prajunvelandi@gmail.com>
2024-07-01 14:10:05 +0530

    added files reverse in python and change text inside text

---
+++ change text inside text.py
@@ -0,0 +1,5 @@
+text1 = "Mered and Bored have a red balloon and a red carpet"
+print(text1.replace('red', 'blue')) # in this case we will not get desired output because it even change word red inside the names also
+
+text2 = "Mered and Bored have a red balloon and a red carpet"
+print(text2.replace(' red', ' blue')) # In this way we get a desired output.
---
+++ reverse in python.py
@@ -0,0 +1,7 @@
+a = [1, 2, 3, 4, 5]
+b = [6, 7, 8, 9, 10]
+c = 'hello world'
+a.reverse()
+print(a)
+print(b[::-1])
+print(c[::-1])
