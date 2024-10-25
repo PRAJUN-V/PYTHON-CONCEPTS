ea58964fcaf5012e8a87548c1f3f30b2c1c79a6f
prajun v <prajunvelandi@gmail.com>
2024-10-25 08:06:02 +0530

    updated Dictionary obj to database file

--- Save Dicionary Obj to Database.py
+++ Save Dicionary Obj to Database.py
@@ -1,13 +1,13 @@
 import sqlite3
 
-user = {
-    1: {
+users = {
+    2: {
         'name': 'Prajun V',
         'age': 23,
         'place': 'Thalassery',
         'email': 'prajunvelandi@gmail.com'
     },
-    2: {
+    3: {
         'name': 'Alex',
         'age': 24,
         'place': 'Kannur',
@@ -20,16 +20,21 @@ user = {
 #   If the file does not exist, it creates a new SQLite database file named user.db.
 connection = sqlite3.connect('user.db')
 
-curser = connection.cursor() # To establish connection between database.
-curser.execute(
+cursor = connection.cursor() # To establish connection between database.
+cursor.execute(
     #  Write sql queries here.
     'CREATE TABLE IF NOT EXISTS user(id int primary key, name varchar(100) not null, age int not null, place varchar(100) not null, email varchar(100))'
 )
 
 # curser.execute("INSERT INTO user (id, name, age, place, email) values (1, 'anu', 23, 'Thalassey', 'anu@gmail.com')")
 
-curser.execute("SELECT * FROM user")
-rows = curser.fetchall()
+# for id, user in users.items():
+#     print(id)
+#     print(user)
+#     cursor.execute(f"INSERT INTO user (id, name, age, place, email) values ('{id}', '{user['name']}', {user['age']}, '{user['place']}', '{user['email']}')")
+
+cursor.execute("SELECT * FROM user")
+rows = cursor.fetchmany(1)
 print(rows)
 
 connection.commit()  # Save the changes
